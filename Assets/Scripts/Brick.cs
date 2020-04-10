6ba0af8b2c46ba091d1c69ebc96cae011ebcad9c
Jamee Kim <jameekim@nodaplife.me>
2020-04-10 12:51:56 -0400

    Create icons for power ups

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -3,9 +3,13 @@
 [RequireComponent(typeof(Collider2D))]
 public class Brick : MonoBehaviour
 {
+    public BrickSettings settings;
+
     private void OnCollisionEnter2D(Collision2D other)
     {
-        Destroy(gameObject);
+        if (settings.RandomPowerUpByChance(out GameObject powerUp))
+            Instantiate(powerUp, transform.position, Quaternion.identity);
         GameController.Instance.IncrementScore();
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/BrickSettings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "BrickSettings", menuName = "New Brick Settings", order = 0)]
+public class BrickSettings : ScriptableObject
+{
+    [Range(0.0f, 1.0f)]
+    public float powerUpChance = 0.01f;
+
+    public GameObject[] powerUps;
+
+    public bool RandomPowerUpByChance(out GameObject newGameObject)
+    {
+        bool shouldBeCreated = Random.Range(0.0f, 1.0f) < powerUpChance;
+        newGameObject = shouldBeCreated ? powerUps[Random.Range(0, powerUps.Length)] : null;
+        return shouldBeCreated;
+    }
+}
